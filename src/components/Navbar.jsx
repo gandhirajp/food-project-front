9a928c441af3b757e3c13b472522dfb3fab7c087
gandhirajp <gandhiraj007p@gmail.com>
2022-03-24 19:54:16 +0530

    four

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { logoutUser } from '../actions/userActions';
 
 function Navbar() {
@@ -12,9 +13,9 @@ function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-sm shadow-lg p-3 mb-5 navbarr rounded ">
-                <a className="navbar-brand"  href="/">
+                <Link className="navbar-brand" to="/">
                     DNC FOOD SHOP
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -34,17 +35,17 @@ function Navbar() {
                             </div>
                         ) : (
                             <li className="nav-item ">
-                                <a className="nav-link" href="/login">
+                                <Link className="nav-link" to="/login">
                                     Login 
-                                </a>
+                                </Link>
                             </li>
                         )}
 
 
                         <li className="nav-item">
-                            <a className="nav-link" href="/cart">
+                            <Link className="nav-link" to="/cart">
                                 Cart {cartstate.cartItems.length}
-                            </a>
+                            </Link>
                         </li>
 
                     </ul>
