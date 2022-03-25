3a03a6e75a192d937f85142989f4c4818908bb26
gandhirajp <gandhiraj007p@gmail.com>
2022-03-25 10:54:29 +0530

    last

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto mx-4 text-white">
+                    <ul className="navbar-nav ml-auto mx-4 ">
 
                         {/* current user name */}
                         {currentUser ? (
